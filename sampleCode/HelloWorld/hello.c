a297b65ab712d75d50a0ca65e65de09c3358598c
Curtis Lin <cococany@gmail.com>
2017-03-12 16:59:35 +0800

    add a hello world sample code, which include a simple make file

---
+++ sampleCode/HelloWorld/hello.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+#define nameLength 20
+
+void main()
+{
+	char userName[nameLength];
+
+	printf("This is a sample code for env build start, please tell me who u r:\n");
+	scanf("%s", userName);
+	printf("welcome %s! please start ur programing here, have a nice trip! \n", userName);
+
+	return;
+}
